519642021faacb5984b8aa2b864b5ac218b7663e
otegami <a.s.takuya1026@gmail.com>
2022-04-25 08:23:49 +0900

    learn signal

--- signal.rb
+++ signal.rb
@@ -1,30 +1,47 @@
-child_processes = 3
-dead_processes = 0
+# child_processes = 3
+# dead_processes = 0
 
-child_processes.times do
-  fork do
-    sleep 3
-  end
-end
+# child_processes.times do
+#   fork do
+#     sleep 3
+#   end
+# end
 
-$stdout.sync = true
+# $stdout.sync = true
 
-trap(:CHLD) do
-  begin
-    while pid = Process.wait(-1, Process::WNOHANG)
-      puts pid
-      dead_processes += 1
-    end
-  rescue Errno::ECHILD
-  end
-end
+# trap(:CHLD) do
+#   begin
+#     while pid = Process.wait(-1, Process::WNOHANG)
+#       puts pid
+#       dead_processes += 1
+#     end
+#   rescue Errno::ECHILD
+#   end
+# end
 
-loop do
-  exit if dead_processes == child_processes
-  sleep 1
-end
+# loop do
+#   exit if dead_processes == child_processes
+#   sleep 1
+# end
 
-loop do
-  puts (Math.sqrt(rand(44)) ** 8).floor
-  sleep 1
-end
+# loop do
+#   puts (Math.sqrt(rand(44)) ** 8).floor
+#   sleep 1
+# end
+
+puts pid = Process.pid
+trap(:INT) { print "Na na na, you can't get me." }
+sleep
+
+Process.kill(:INT, pid)
+Process.kill(:KILL, pid)
+
+trap(:INT) { puts 'This is the first signal handler' }
+
+old_handler = trap(:INT) {
+  old_handler.call
+  puts 'This is the second handler'
+  exit
+}
+
+sleep
