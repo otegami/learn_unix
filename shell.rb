1e50f3922b4931443d5162ef0d3d22e428e00730
otegami <a.s.takuya1026@gmail.com>
2022-04-26 08:15:21 +0900

    learn the differences between exec and fork

---
+++ shell.rb
@@ -0,0 +1,7 @@
+# exec 'ls', '--help'
+
+hosts = File.open('/etc/hosts')
+
+python_code = %Q[import os; print os.fdopen(#{hosts.fileno}).read()]
+
+exec 'python', '-c', python_code, {hosts.fileno => hosts}
